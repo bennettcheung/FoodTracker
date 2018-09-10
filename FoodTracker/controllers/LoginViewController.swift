900b96a204a7e9d10e5309cb23086492e7b54729
Bennett Cheung <bennettcheung@yahoo.com>
2018-09-10 14:13:25 -0700

    Added ability to login and signup using the Cloudtracker API

---
+++ FoodTracker/controllers/LoginViewController.swift
@@ -0,0 +1,129 @@
+// Copyright (c) 2017 Lighthouse Labs. All rights reserved.
+// 
+// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+// distribute, sublicense, create a derivative work, and/or sell copies of the
+// Software in any work that is designed, intended, or marketed for pedagogical or
+// instructional purposes related to programming, coding, application development,
+// or information technology.  Permission for such use, copying, modification,
+// merger, publication, distribution, sublicensing, creation of derivative works,
+// or sale is expressly withheld.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  
+  @IBOutlet weak var usernameTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  private var savedToken = ""
+  private let usernameKey = "username"
+  private let passwordKey = "password"
+  private let tokenKey = "token"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  override func viewDidAppear(_ animated: Bool) {
+    attemptToAutoLogin()
+    
+  }
+  
+  
+  func attemptToAutoLogin() {
+    //check user default for saved user information
+    guard let username = UserDefaults.standard.value(forKey: usernameKey ) as? String,
+      let password = UserDefaults.standard.value(forKey: passwordKey) as? String,
+      let token = UserDefaults.standard.value(forKey: tokenKey) as? String else{
+        //try to login
+        print("No user default username and password")
+        return
+    }
+    usernameTextField.text = username
+    passwordTextField.text = password
+    savedToken = token
+  }
+
+  func showLoginErrorAlert(message: String) {
+    //show alert if there is an error
+    let alert = UIAlertController(title: "Login error", message: message, preferredStyle: .alert)
+    
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    
+    self.present(alert, animated: true)
+  }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+  
+  func validUsernamePassword() -> Bool{
+    if let username = usernameTextField.text, username.contains(" "){
+      showLoginErrorAlert(message: "User name can't contain spaces!")
+      return false
+    }
+    
+    if let password = passwordTextField.text, password.count < 6
+    {
+      showLoginErrorAlert(message: "Password length has to be longer than 6 characters!")
+      return false
+    }
+    
+    return true
+  }
+
+  // MARK: - IB Actions
+
+  @IBAction func loginUser(_ sender: Any) {
+    if validUsernamePassword() {
+      
+      guard let username =  usernameTextField.text,
+        let password = passwordTextField.text else {
+          return
+      }
+
+    
+      //try first time, then try to sign up on the server
+      
+      if savedToken == ""{
+        CloudTrackerManager.shared.signupUser(username: username, password: password, completion: { (token) -> (Void) in
+          //save credentials in userdefault
+          UserDefaults.standard.set(username, forKey: self.usernameKey)
+          UserDefaults.standard.set(password, forKey: self.passwordKey)
+          UserDefaults.standard.set(token, forKey: self.tokenKey)
+        })
+          
+      }
+      else{
+      
+        CloudTrackerManager.shared.loginUser(username: username, password: password, completion: { (token) -> (Void) in
+          //save credentials in userdefault
+          UserDefaults.standard.set(username, forKey: self.usernameKey)
+          UserDefaults.standard.set(password, forKey: self.passwordKey)
+          UserDefaults.standard.set(token, forKey: self.tokenKey)
+          
+          self.performSegue(withIdentifier: "segueToMainScreen", sender: self)
+        })
+      }
+      
+      
+
+      
+    }
+  }
+}
+
---
+++ FoodTracker/models/CloudTrackerManager.swift
@@ -0,0 +1,110 @@
+//
+//  CloudTrackerManager.swift
+//  FoodTracker
+//
+//  Created by Bennett on 2018-09-10.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+import UIKit
+
+class CloudTrackerManager {
+  
+  static let shared = CloudTrackerManager()
+  
+  private let signupEndpointURL = "https://cloud-tracker.herokuapp.com/signup"
+  private let loginEndpointURL = "https://cloud-tracker.herokuapp.com/login"
+  private let tokenKey = "token"
+  
+  
+  func post(data: [String: Any], toEndpoint: String, completion: @escaping  (Data?, NSError?)->(Void)){
+    guard let postJSON = try? JSONSerialization.data(withJSONObject: data, options: []) else {
+      print("could not serialize json")
+      return
+    }
+    
+    let url = URL(string: toEndpoint)!
+    let request = NSMutableURLRequest(url: url)
+    request.httpBody = postJSON
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    let task = URLSession.shared.dataTask(with: request as URLRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+      
+      guard let data = data else {
+        print("no data returned from server \(String(describing: error?.localizedDescription))")
+        return
+      }
+      
+      guard let response = response as? HTTPURLResponse else {
+        print("no response returned from server \(String(describing: error))")
+        return
+      }
+      guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as! Dictionary<String, Any> else {
+        print("data returned is not json, or not valid")
+        return
+      }
+      
+      guard response.statusCode == 200 else {
+        // handle error
+        print("an error occurred \(String(describing: json["error"]))")
+        return
+      }
+      
+      // do something with the json object
+      completion(data, nil)
+      
+    }
+    
+    task.resume()
+    
+  }
+  
+  func signupUser(username: String, password: String , completion: @escaping  (String?)->(Void)) -> Void {
+    let postData:[String: Any] = [
+      "username": username,
+      "password": password
+    ]
+    
+    post(data: postData, toEndpoint: signupEndpointURL) { (data, error) -> (Void) in
+      //do something with the data
+      print("handle signup api data from signupUser()")
+      
+      guard let data = data, let json = try? JSONSerialization.jsonObject(with: data, options: []) as? Dictionary<String, Any> else {
+        print("data returned is not json, or not valid")
+        return
+      }
+      if let token = json?[self.tokenKey] as? String {
+        print("\(username) - Token is \(token)")
+        completion(token)
+      }
+      
+    }
+
+  }
+
+  func loginUser(username: String, password: String, completion: @escaping  (String?)->(Void)) -> Void {
+    let postData:[String: Any] = [
+      "username": username,
+      "password": password
+    ]
+    
+    post(data: postData, toEndpoint: loginEndpointURL) { (data, error) -> (Void) in
+      //do something with the data
+      print("handle login api data from loginUser()")
+      guard let data = data, let json = try? JSONSerialization.jsonObject(with: data, options: []) as? Dictionary<String, Any> else {
+        print("data returned is not json, or not valid")
+        return
+      }
+      if let token = json?[self.tokenKey] as? String {
+        print("username \(username), token is \(token)")
+        completion(token)
+      }
+      
+      
+      
+    }
+    
+    return
+  }
+  
+}
